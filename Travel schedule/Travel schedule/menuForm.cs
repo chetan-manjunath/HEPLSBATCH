52aba013b2afda2ab0e4cd6178cc1a5c99f9858a
Chetan Manjunath <chetan.manjunath@oneadvanced.com>
2019-08-29 15:19:01 +0530

    new model created

--- Travel schedule/Travel schedule/menuForm.cs
+++ Travel schedule/Travel schedule/menuForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Travel_schedule
 {
-    public partial class Form1 : Form
+    public partial class menuForm : Form
     {
-        public Form1()
+        public menuForm()
         {
             InitializeComponent();
         }
