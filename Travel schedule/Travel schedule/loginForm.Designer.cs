402cbc0d025c4f9bfc4f8c99060f18697dd2e849
Chetan Manjunath <chetan.manjunath@oneadvanced.com>
2019-08-30 14:49:10 +0530

    handeled the exception in login page

--- Travel schedule/Travel schedule/loginForm.Designer.cs
+++ Travel schedule/Travel schedule/loginForm.Designer.cs
@@ -42,9 +42,9 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(460, 98);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(319, 24);
+            this.label1.Size = new System.Drawing.Size(314, 24);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Log in to Advanced Travell Portal";
+            this.label1.Text = "Log in to Advanced Travel Portal";
             // 
             // label2
             // 
